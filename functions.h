90bd9cbb0355e5da4f7077c616789eb58df2ce74
SolarFlare <29712174+SolarDawn@users.noreply.github.com>
2018-01-26 18:30:49 +0000

    Changed int's to floats in divideNumbers()

---
+++ functions.h
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   functions.h
+ * Author: SolarFlare
+ *
+ * Created on 26 January 2018, 18:27
+ */
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+int menu();
+int addNumbers();
+int subtractNumbers();
+int multiplyNumbers();
+float divideNumbers();
+
+#endif /* FUNCTIONS_H */
+
